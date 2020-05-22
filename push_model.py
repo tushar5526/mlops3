4686ecb6439dcded88b519436caa09bcff6056d7
tushar5526 <30565750+tushar5526@users.noreply.github.com>
2020-05-22 17:06:13 +0530

    Add files via upload

---
+++ push_model.py
@@ -0,0 +1,25 @@
+import smtplib, ssl
+
+port = 587  # For starttls
+smtp_server = "smtp.gmail.com"
+sender_email = "codingid6@gmail.com"
+receiver_email = "codingid6@gmail.com"
+password = 'ilovecooding'
+message = """\
+Subject: Final Model Build 
+
+Pull it from GitHub along with history file, that is saved using pickle module
+
+
+"""
+
+context = ssl.create_default_context()
+with smtplib.SMTP(smtp_server, port) as server:
+    server.ehlo()  # Can be omitted
+    server.starttls(context=context)
+    server.ehlo()  # Can be omitted
+    server.login(sender_email, password)
+    server.sendmail(sender_email, receiver_email, message)
+
+
+#upload the model on google drive and share the link with mail
